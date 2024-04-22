bbbfc2469e42ef9c14f131c50892bd1921a94f85
keuhzz <zhukecn@outlook.com>
2024-04-23 00:15:42 +0800

    init maven project

---
+++ src/main/java/MiniMybatisStarter.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * mini mybatis starter.
+ */
+public class MiniMybatisStarter {
+    private static final Logger log = LoggerFactory.getLogger(MiniMybatisStarter.class);
+
+    public static void main(String[] args) {
+        log.info("Hello, Welcome to mini-mybatis!");
+    }
+}

4b9d3a7bb60fe34535943a8b62b77c66087f6a1d
keuhzz <zhukecn@outlook.com>
2024-04-25 23:04:24 +0800

    feat: [WIP] Mapper XML解析 - 提前准备单元测试 XML 配置文件

--- src/test/java/com/zyuhoo/mini/mybatis/dao/UserDao.java
+++ src/test/java/com/zyuhoo/mini/mybatis/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.zyuhoo.mini.mybatis.dao;
 
+import com.zyuhoo.mini.mybatis.entity.User;
+
 /**
  * Provide dao interface test.
  */
@@ -7,4 +9,6 @@ public interface UserDao {
     String queryUserName(String userId);
 
     String queryUsrAge(String userId);
+
+    User queryUserInfoById(Long id);
 }
